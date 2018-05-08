2ee8023ce7638ff4cc6996180b01ef82d48144df
kentOneR <blot.quentin@gmail.com>
2018-05-08 10:24:50 +0200

    Add social media font

--- footer.php
+++ footer.php
@@ -1,38 +1,54 @@
-        <div id="scroll-top">
-            <img src="img/asset/big-arrow-up.svg" alt="Flèche vers le haut">
+<div id="scroll-top">
+    <img src="img/asset/big-arrow-up.svg" alt="Flèche vers le haut">
+</div>
+<footer>
+    <form action="">
+        <label for="newsletter-subscribe">Pour recevoir nos offres spéciales inscrivez-vous à la newsletter</label>
+        <input type="email" name="newsletter-subscribe" placeholder="Votre adresse mail">
+        <button class="button-visual" type="submit" data-hover="JE M'INSCRIS" data-active="JE M'INSCRIS">
+            <span>INSCRIVEZ-VOUS</span>
+        </button>
+    </form>
+    <div class="social-media">
+        <div class="social-icons">
+            <a href="https://fr-fr.facebook.com/">
+                <i class="fa fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com/?lang=fr">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a href="https://www.instagram.com/?hl=fr">
+                <i class="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.youtube.com/">
+                <i class="fa fa-youtube" aria-hidden="true"></i>
+            </a>
         </div>
-        <footer>
-            <form action="">
-                <label for="newsletter-subscribe">Pour recevoir nos offres spéciales inscrivez-vous à la newsletter</label>
-                <input type="email" name="newsletter-subscribe" placeholder="Votre adresse mail">
-                <button class="button-visual" type="submit" data-hover="JE M'INSCRIS" data-active="JE M'INSCRIS">
-                    <span>INSCRIVEZ-VOUS</span>
-                </button>
-            </form>
-            <ul class="footer-nav">
-                <li class="footer-link">
-                    <a href="contact.php">Accès & Contact</a>
-                </li>
-                <li class="footer-link">
-                    <a href="mention-legale.php">Mentions Légales</a>
-                </li>
-                <li class="footer-link">
-                    <a href="cgvente.php">Conditions Générales de Vente</a>
-                </li>
-                <li class="footer-link">
-                    <a href="#">Plan du site</a>
-                </li>
-                <li>&#169; Parimis hôtel 2018 </li>
-            </ul>
-        </footer>
     </div>
+    <ul class="footer-nav">
+        <li class="footer-link">
+            <a href="contact.php">Accès & Contact</a>
+        </li>
+        <li class="footer-link">
+            <a href="mention-legale.php">Mentions Légales</a>
+        </li>
+        <li class="footer-link">
+            <a href="cgvente.php">Conditions Générales de Vente</a>
+        </li>
+        <li class="footer-link">
+            <a href="#">Plan du site</a>
+        </li>
+        <li>&#169; Parimis hôtel 2018 </li>
+    </ul>
+</footer>
+</div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.2/js/swiper.min.js"></script>
-    <script src="https://unpkg.com/js-datepicker"></script>
-    <script src="js/main.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIAcWhcKEJXg7m1jfn_AbVDhZyXjXEn1I&callback=initMap" async
-        defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.2/js/swiper.min.js"></script>
+<script src="https://unpkg.com/js-datepicker"></script>
+<script src="js/main.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIAcWhcKEJXg7m1jfn_AbVDhZyXjXEn1I&callback=initMap" async
+    defer></script>
 </body>
 
 </html>
