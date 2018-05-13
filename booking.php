45609d9c0fe2526a9abbb23f6e2e9af854cd3067
kentOneR <blot.quentin@gmail.com>
2018-05-13 13:21:07 +0200

    Booking : add thumbnails

--- booking.php
+++ booking.php
@@ -76,13 +76,118 @@
         </aside>
         <section class="room-result col-xl-9">
             <div class="room-detail">
-                <img src="img/hotel/room/big/room-1.jpg" alt="Intérieur chambre Luxury in Advance">
+                <div class="room-visual">
+                    <div class="main-visual">
+                        <img src="img/hotel/room/big/room-1.jpg" alt="Intérieur chambre Luxury in Advance">
+                    </div>
+                    <div class="all-thumbnail">
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="room-content">
+                    <h4>CHAMBRE : CONFORT</h4>
+                    <h3>Escape Suite</h3>
+                    <p>TAILLE : 35 - 40 m²</p>
+                    <p>à partir de 765 € par nuit</p>
+                    <p>Conçues pour des voyageurs modernes en quête de confort absolu, ces chambres font partie des plus spacieuses
+                        de Paris et sont équipées d’une technologie de pointe.</p>
+                    <div class="star-ratings-sprite">
+                        <span style="width:87%" class="star-ratings-sprite-rating"></span>
+                    </div>
+                    <div class="booking">
+                        <a class="underline--magical" href="#">RÉSERVER</a>
+                    </div>
+                </div>
             </div>
             <div class="room-detail">
-                <img src="img/hotel/room/big/room-2.jpg" alt="Terrasse chambre Rooftop Garden">
+                <div class="room-visual">
+                    <div class="main-visual">
+                        <img src="img/hotel/room/big/room-2.jpg" alt="Terrasse chambre Rooftop Garden">
+                    </div>
+                    <div class="all-thumbnail">
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="room-content">
+                    <h4>CHAMBRE : CONFORT</h4>
+                    <h3>Escape Suite</h3>
+                    <p>TAILLE : 35 - 40 m²</p>
+                    <p>à partir de 765 € par nuit</p>
+                    <p>Conçues pour des voyageurs modernes en quête de confort absolu, ces chambres font partie des plus spacieuses
+                        de Paris et sont équipées d’une technologie de pointe.</p>
+                    <div class="star-ratings-sprite">
+                        <span style="width:87%" class="star-ratings-sprite-rating"></span>
+                    </div>
+                    <div class="booking">
+                        <a class="underline--magical" href="#">RÉSERVER</a>
+                    </div>
+                </div>
             </div>
             <div class="room-detail">
-                <img src="img/hotel/room/big/room-3.jpg" alt="Intérieur chambre Suite Royale">
+                <div class="room-visual">
+                    <div class="main-visual">
+                        <img src="img/hotel/room/big/room-3.jpg" alt="Intérieur chambre Suite Royale">
+                    </div>
+                    <div class="all-thumbnail">
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                        <div class="room-thumbnail">
+                            <img src="http://via.placeholder.com/100x70" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="room-content">
+                    <h4>CHAMBRE : CONFORT</h4>
+                    <h3>Escape Suite</h3>
+                    <p>TAILLE : 35 - 40 m²</p>
+                    <p>à partir de 765 € par nuit</p>
+                    <p>Conçues pour des voyageurs modernes en quête de confort absolu, ces chambres font partie des plus spacieuses
+                        de Paris et sont équipées d’une technologie de pointe.</p>
+                    <div class="star-ratings-sprite">
+                        <span style="width:87%" class="star-ratings-sprite-rating"></span>
+                    </div>
+                    <div class="booking">
+                        <a class="underline--magical" href="#">RÉSERVER</a>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
