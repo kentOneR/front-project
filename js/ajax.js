1ff83a168bb58b5816fff55aaf1968378242cb45
kentOneR <blot.quentin@gmail.com>
2018-05-14 10:40:12 +0200

    Add visual Swiper overlay

--- js/ajax.js
+++ js/ajax.js
@@ -72,12 +72,17 @@
         speed: 1200,
         slidesPerView: 1,
         spaceBetween: 0,
+        watchOverflow: true,
         autoplay: {
             delay: 6000
         },
         pagination: {
             el: '.swiper-pagination',
             clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         }
     });
 
