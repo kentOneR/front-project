75e508f702cca4c148f11a46bac9d6793aef7d08
kentOneR <blot.quentin@gmail.com>
2018-05-15 11:19:45 +0200

    header : add 5 stars

--- header.php
+++ header.php
@@ -1,6 +1,14 @@
-
-        <header>
-            <div class="parimis-head">
+<header>
+    <div class="parimis-head">
+        <div class="header-container">
             <img src="img/asset/logo/parimis-logo-white.svg" alt="Logo Parimis blanc">
+            <div class="five-stars">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
             </div>
-        </header>
+        </div>
+    </div>
+</header>
