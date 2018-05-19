4101ff09332c6ac25cab64229d054b80d9ee1cc6
Ubuntu <blot.quentin@gmail.com>
2018-05-19 12:44:22 +0200

    Booking: room-content

--- hotel.php
+++ hotel.php
@@ -52,25 +52,25 @@
         <div class="all-video-container">
             <div class="col-sm-12 col-lg-6 col-xl-3">
                 <div class="video-container">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/_mtIU2l6Nqk" frameborder="0" allow="autoplay; encrypted-media"
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/_mtIU2l6Nqk" frameborder="0" allow="encrypted-media"
                         allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-sm-12 col-lg-6 col-xl-3">
                 <div class="video-container">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pyvqW6X4peI" frameborder="0" allow="autoplay; encrypted-media"
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pyvqW6X4peI" frameborder="0" allow="encrypted-media"
                         allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-sm-12 col-lg-6 col-xl-3">
                 <div class="video-container">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wYHbv4Fpx_I" frameborder="0" allow="autoplay; encrypted-media"
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wYHbv4Fpx_I" frameborder="0" allow="encrypted-media"
                         allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-sm-12 col-lg-6 col-xl-3">
                 <div class="video-container">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rAOXN35S93s" frameborder="0" allow="autoplay; encrypted-media"
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rAOXN35S93s" frameborder="0" allow="encrypted-media"
                         allowfullscreen></iframe>
                 </div>
             </div>
